9146868342b04b0abeae8e28b782ba4335421295
SujitPatil <patilsujit205@gmail.com>
2024-08-17 20:08:47 +0530

    new project

---
+++ src/main/java/com/sud/api/DigitalApiApplication.java
@@ -0,0 +1,13 @@
+package com.sud.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sud/api/DigitalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sud.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
